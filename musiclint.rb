bb95c465e69cf08eec556608c1b92a47bf082842
Pistos <git dawt pistos aet purepistos dawt net>
2019-07-30 16:31:14 -0500

    Fix display string for Notes

--- musiclint.rb
+++ musiclint.rb
@@ -89,11 +89,19 @@ module MusicLint
       'A' => 9,
       'B' => 11,
     }
+    ACCIDENTAL_FOR = {
+      -2 => 'bb',
+      -1 => 'b',
+      0 => '',
+      1 => '#',
+      2 => '##',
+    }
 
     attr_reader :to_i, :to_s
 
     def initialize(xml_node:)
       @octave = xml_node.at('octave').content.to_i
+
       alter_node = xml_node.at('alter')
       if alter_node
         alter = alter_node.content.to_i
@@ -101,9 +109,12 @@ module MusicLint
         alter = 0
       end
       step = xml_node.at('step').content
+      accidental = ACCIDENTAL_FOR[alter]
+
+      display_step = "#{step}#{accidental}"
 
       @to_i = @octave * 12 + STEP_TO_INT[step] + alter
-      @to_s = "#{step}#{@octave}"
+      @to_s = "#{display_step}#{@octave}"
     end
   end
 
