a2a82d843185ed6b8d8ae9a25a7022f518b2a0f7
Pistos <git dawt pistos aet purepistos dawt net>
2019-08-03 19:13:15 -0500

    Refactor Measure#parse

--- lib/musiclint/measure.rb
+++ lib/musiclint/measure.rb
@@ -10,6 +10,19 @@ module MusicLint
   class Measure
     attr_reader :number
 
+    private def add_notes(moment:, notes_to_add:)
+      if notes_to_add.any?
+        t = moment.time_in_measure
+        @chords[t] = @chords[t].merge(
+          Chord.new(moment: moment).tap { |chord|
+            notes_to_add.each do |note|
+              chord.add_note(note)
+            end
+          }
+        )
+      end
+    end
+
     def chords
       @chords.keys.sort.map { |time|
         @chords[time]
@@ -21,13 +34,13 @@ module MusicLint
       @chords = Hash.new { |h, k| h[k] = NilChord.new }
     end
 
-    def notes_sounding_at(divisions:, notes_starting_at:, time_of_sound:)
+    private def notes_sounding_at(divisions:, notes_by_time:, time_of_sound:)
       notes = {}
-      times = notes_starting_at.keys.sort
+      times = notes_by_time.keys.sort
 
       times.each do |t|
         if t <= time_of_sound
-          notes_starting_at[t].find_all(&:note?).each do |note|
+          notes_by_time[t].find_all(&:note?).each do |note|
             if t + note.duration / divisions > time_of_sound
               notes[note.voice] = note
             end
@@ -39,55 +52,73 @@ module MusicLint
     end
 
     def parse(measure_node:, part_id:)
-      @number ||= measure_node.attribute('number').content.to_i
-
-      if measure_node.at('attributes/divisions')
-        @divisions ||= BigDecimal(measure_node.at('attributes/divisions').content)
-      end
+      parse_number(measure_node: measure_node)
+      parse_divisions(measure_node: measure_node)
 
       if @divisions
-        total_duration = Hash.new(BigDecimal("0"))
-        notes_starting_at ||= Hash.new { |h, k| h[k] = [] }
-
-        measure_node.xpath('note').map { |n|
-          Note.new(part_id: part_id, xml_node: n)
-        }.each do |note|
-          voice = "#{part_id}-#{note.voice}"
-          time = total_duration[voice]
-          total_duration[voice] += note.duration / @divisions
-          notes_starting_at[time] << note
-        end
-
-        times = notes_starting_at.keys.sort
-        times.each do |t|
-          notes_to_add = notes_sounding_at(
-            divisions: @divisions,
-            notes_starting_at: notes_starting_at,
-            time_of_sound: t
-          )
-
-          if notes_to_add.any?
-            moment = Moment.new(measure_number: @number, time_in_measure: t)
-
-            existing_moment = @chords[t].moment
-            if existing_moment && existing_moment != moment
-              raise ChordMomentMismatchError.new(
-                "Will not merge chords from different moments. #{existing_moment} != #{moment}"
-              )
-            end
-
-            @chords[t] = @chords[t].merge(
-              Chord.new(moment: moment).tap { |chord|
-                notes_to_add.each do |note|
-                  chord.add_note(note)
-                end
-              }
-            )
-          end
-        end
+        parse_notes(measure_node: measure_node, part_id: part_id)
       end
 
       @divisions
+    end
+
+    private def parse_divisions(measure_node:)
+      if measure_node.at('attributes/divisions')
+        @divisions ||= BigDecimal(measure_node.at('attributes/divisions').content)
+      end
+    end
+
+    private def parse_number(measure_node:)
+      @number ||= measure_node.attribute('number').content.to_i
+    end
+
+    private def parsed_notes(measure_node:, part_id:)
+      measure_node.xpath('note').map { |n|
+        Note.new(part_id: part_id, xml_node: n)
+      }
+    end
+
+    private def parse_notes(measure_node:, part_id:)
+      total_duration = Hash.new(BigDecimal(0))
+      notes_by_time = Hash.new { |h, k| h[k] = [] }
+
+      notes = parsed_notes(measure_node: measure_node, part_id: part_id)
+      notes.each do |note|
+        voice = "#{part_id}-#{note.voice}"
+        time = total_duration[voice]
+        total_duration[voice] += note.duration / @divisions
+        notes_by_time[time] << note
+      end
+
+      process_notes(notes_by_time: notes_by_time)
+    end
+
+    private def process_notes(notes_by_time:)
+      times = notes_by_time.keys.sort
+      times.each do |t|
+        process_notes_at(notes_by_time: notes_by_time, time: t)
+      end
+    end
+
+    private def process_notes_at(notes_by_time:, time:)
+      notes_to_add = notes_sounding_at(
+        divisions: @divisions,
+        notes_by_time: notes_by_time,
+        time_of_sound: time
+      )
+
+      moment = Moment.new(measure_number: @number, time_in_measure: time)
+      require_same_moment(moment, @chords[time].moment)
+
+      add_notes(notes_to_add: notes_to_add, moment: moment)
+    end
+
+    private def require_same_moment(moment1, moment2)
+      if moment2 && moment2 != moment1
+        raise ChordMomentMismatchError.new(
+          "Will not merge chords from different moments. #{moment2} != #{moment1}"
+        )
+      end
     end
   end
 end
